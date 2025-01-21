cb7735c16e898f8e98c30ed6fe5b5ba9296e360b
Rahul-Mehar <rahulmehar92@gmail.com>
2025-01-21 19:33:06 +0530

    comparison

---
+++ 01_basics/comparision.js
@@ -0,0 +1,43 @@
+// console.log(2 > 1); // true
+// console.log(2 >= 1); //true
+// console.log(2 < 1); // false
+// console.log(2 == 1); //false
+// console.log(2 != 1); // true
+
+
+// console.log("2" > 1);// true beacuse its automatically convert into number
+// console.log("02" > 1); // true
+
+
+
+
+// console.log(null > 0); // false
+// console.log(null == 0); // false
+// console.log(null >= 0); // true
+
+
+
+// console.log(undefined == 0); // false
+// console.log(undefined > 0); // false
+// console.log(undefined < 0); // false
+
+// NOTE : in terms of comparison we mostly avoid all above comparison type
+
+
+// === this is called strictly checked beacuse its compare that data type also that by
+
+console.log("2" === 2); //false
+
+
+
+
+
+
+
+
+
+
+
+
+
+
