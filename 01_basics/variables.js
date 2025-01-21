709afea8f8fd12b946dfd3d4029800735dbcb614
Rahul-Mehar <rahulmehar92@gmail.com>
2025-01-21 10:14:46 +0530

    update

---
+++ 01_basics/variables.js
@@ -0,0 +1,24 @@
+const accountId = 144553
+let accountEmail = "Rahul@12345"
+var accountPassword = "12345"
+accountCity = "jaipur"
+
+
+accountEmail = "Rahi@13324"
+accountPassword = "2424324"
+accountCity = "pune"
+
+let accountState;
+
+// accountId = 2 // not allowed
+
+console.log(accountId);
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState]);
+
+// console.log(accountEmail);
+// console.log(accountPassword);
+// console.log(accountCity);
+
+/* prefer not use var 
+beacuse of issue in block scope and function scope
+*/
