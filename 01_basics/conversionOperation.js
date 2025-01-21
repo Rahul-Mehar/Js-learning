fc30cd2eb808989e5d11d88fddce909f667913be
Rahul-Mehar <rahulmehar92@gmail.com>
2025-01-21 19:17:21 +0530

    operation

--- 01_basics/conversionOperation.js
+++ 01_basics/conversionOperation.js
@@ -35,11 +35,53 @@
 
 
 
-let someNumber = 33
+// let someNumber = 33
 
-let stringNumber =String(someNumber)
-console.log(stringNumber);
-console.log(typeof stringNumber);
+// let stringNumber =String(someNumber)
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+
+
+// ********************* Operations ****************
+
+// let value = 3
+// let negValue = -value
+// console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3);
+// console.log(2/3);
+// console.log(2%3);
+
+
+// let str1 = "hello";
+// let str2 = " Rahul"
+
+// let str3 = str1 + str2;
+// console.log(str3);
+
+
+// console.log("1" + 2);// 12
+// console.log(1 + "2");// 12 
+// console.log("1" + 2 + 2 ); // 122 beacuse first number is string that by its all accept string 
+// console.log(1 + 2 + "2" ); //32 beacuse its first number is number that by its add it
+
+
+// console.log(+true);// 1
+// console.log(+"");// 0 
+// console.log(true+) // not allowed error 
+
+let num1, num2, num3 
+
+num1 = num2 = num3 = 2+2
+
+let gamecounter = 100
+gamecounter++;// here ++ is presidance operator
+console.log(gamecounter);
+
+
 
 
 
