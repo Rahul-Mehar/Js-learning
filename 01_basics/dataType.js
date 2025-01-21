bae5c165efcf289835c6552ae6f109a6e9aab2b8
Rahul-Mehar <rahulmehar92@gmail.com>
2025-01-21 10:37:25 +0530

    update

---
+++ 01_basics/dataType.js
@@ -0,0 +1,38 @@
+"use strict"; // treat all js as newer version
+
+// alert("hello") // we are using nodejs not browser that by its shows error
+
+
+let name = "Rahul"
+let age = 30
+let isLoggedIn = false
+let State;
+
+
+// number = 2 tp power 53
+// bigint
+// string =""
+// boolean = true/false
+// null = stanalone value
+// undefined ==>
+//  symbol ==> unique
+
+// object
+
+console.log(typeof "Rahul");
+console.log(typeof age);
+console.log(typeof undefined);
+console.log(typeof null);
+
+
+
+
+
+
+
+
+
+
+
+
+
