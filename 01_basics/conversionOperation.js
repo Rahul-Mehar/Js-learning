45624e471ec5015ee7f2f490a20b534448da0c98
Rahul-Mehar <rahulmehar92@gmail.com>
2025-01-21 18:52:44 +0530

    converstion of datatype

---
+++ 01_basics/conversionOperation.js
@@ -0,0 +1,45 @@
+// let score = 33;
+
+// console.log(typeof score);
+// console.log(typeof (score));
+
+
+// here string is conveting in number datatype
+// let score = "33";
+// let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+
+
+// let score = "33abc";
+// let valueInNumber = Number(score);
+// console.log(valueInNumber);
+// console.log(typeof valueInNumber);
+
+
+
+
+// "33" = 33
+// "33abc" = NaN
+// true = 1
+// false = 0
+
+// let isLoggedIn = 1
+// let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn);
+
+// 1 = true
+// 0 = false
+// "" = false
+// "Rahul" = true
+
+
+
+
+let someNumber = 33
+
+let stringNumber =String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
+
+
+
