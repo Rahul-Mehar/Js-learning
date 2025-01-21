7aa1b303cab68365094419721b5a913be9bf32d4
Rahul-Mehar <rahulmehar92@gmail.com>
2025-01-21 19:53:13 +0530

    datatype summary

---
+++ 01_basics/datatypes-summary.js
@@ -0,0 +1,53 @@
+// primitive (7 types)
+//  String
+//  Number
+//  Boolean
+//  null
+//  undefined
+//  Symbol
+//  BigInt
+
+//javascript are dynamically language
+
+const score = 100// Number
+const scoreValue = 100.3// bigint
+
+const isLoggedIn = false// boolean
+const outsideTemp = null//null
+let usesrEmail; // undefined
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+
+// console.log(id === anotherId);//false
+
+
+const bigNumber = 654645485442n
+
+// console.log(bigNumber);
+// console.log(typeof bigNumber);
+
+
+
+
+//  Reference(Non primitive)
+// Array
+// Object
+// Function
+
+const hero = ["shaktiman", "naagraj", "doga"];
+
+
+let myObj = {
+    name: "Rahul",
+    age: 30,
+}
+
+
+const myFunction = function(){
+console.log("hello world");
+
+}
+
+
+
