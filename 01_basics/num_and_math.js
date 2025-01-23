5fff3459a06b1e6021e1da96f5eaf653b3a52c4c
Rahul-Mehar <rahulmehar92@gmail.com>
2025-01-23 16:54:16 +0530

    update

---
+++ 01_basics/num_and_math.js
@@ -0,0 +1,50 @@
+// const score = 200;
+// console.log(score);//200
+
+
+
+// const balance = new Number(100)
+// console.log(balance);
+
+// console.log(balance.toString().length);
+// console.log(typeof balance);//object
+
+// console.log(balance.toFixed(2));//100.00
+
+
+// const otherNumber = 25.3554
+// console.log(otherNumber.toPrecision(1));// round figure mai kr k deta hai but alag alag
+
+ const hundreds = 10000000;
+// console.log(hundreds.toLocaleString('en-IN'));// digits mai kr k deta hai like 1,00,00,000
+// en-IN iska mtlab hai muje india ka formate kr do or kisi bhi country ka formate nikal skty hai like (us and many more)
+
+/*********** Maths *************/
+
+
+// console.log(Math);
+// console.log(Math.abs(-4));//4
+// console.log(Math.round(4.6));//5
+// console.log(Math.ceil(4.2));//5 because hamesa uppar wali value lega 
+// console.log(Math.floor(4.9));//4 bacuse hamesa niche wali value lega
+// console.log(Math.min(4, 9, 22, 1));//1 
+// console.log(Math.max(4, 9, 22, 1));//22
+
+
+
+// console.log(Math.random());// yhe hamesa rendom value dega (0 se 1 ke bich mai)
+
+// console.log(Math.random()*10 + 1);// yhe hamesa 1-22 ke bich k value dega
+// console.log(Math.random()*10 + 1);// yhe hamesa 1-22 ke bich k value dega
+// console.log(Math.floor(Math.random()*10) + 1);// yhe hamesa 1-22 ke bich k value dega
+
+const min = 10
+const max = 20
+
+console.log(Math.floor(Math.random() * (max - min + 1)) + min
+)
+
+
+
+
+
