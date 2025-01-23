d766a322acaa37eba9d420a52f7106518fbdadfd
Rahul-Mehar <rahulmehar92@gmail.com>
2025-01-23 15:32:02 +0530

    update

---
+++ 01_basics/string.js
@@ -0,0 +1,73 @@
+const name = "Rahul"
+
+const repoCount = 50
+
+// console.log(name + " " +  repoCount);
+// console.log(`hello my name is ${name.touppercase} and my repo count is ${repoCount}`)
+
+
+const gameName = new String('Rahulr')
+
+// console.log(gameName[0]);
+// console.log(gameName[1]);
+// console.log(gameName.__proto__);
+// console.log(gameName.length);
+// console.log(gameName.toUpperCase());
+// console.log(gameName.charAt(2));
+// console.log(gameName.charAt(5));
+// console.log(gameName.indexOf('h'));
+
+
+// const newString = gameName.substring(0, 4)
+// console.log(newString);
+
+// const anotherString = gameName.slice(-5, 3)
+// console.log(anotherString);
+
+
+// const newStringOne = "  Rahul   "
+// console.log(newStringOne);
+// console.log(newStringOne.trim());
+
+
+const url = "https://rahul.com/rahul%20mehar"
+// console.log(url.replace('%20', '-')
+// )
+
+// console.log(url.includes('rahul')
+
+console.log(url.split('.')
+)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
