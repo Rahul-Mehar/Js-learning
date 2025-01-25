8873e9c616837d190e7acdd37f9348afe1370da4
Rahul-Mehar <rahulmehar92@gmail.com>
2025-01-25 08:58:39 +0530

    update

---
+++ 02_basics/array.js
@@ -0,0 +1,55 @@
+// Array
+
+const myArr = [0, 1, 2, 3, 4, 5]
+
+
+const myHeros = ["saktiman", "naagraj"]
+
+const myArr2 = new Array(1, 2, 3, 4)
+console.log(myArr[2]);
+
+
+//array methods
+
+// myArr.push(6) // last mai value add kr dega 
+// myArr.push(7)
+// myArr.pop()// last ki value delete kr dega
+
+
+console.log(myArr);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
