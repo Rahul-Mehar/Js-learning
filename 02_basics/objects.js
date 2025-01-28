b1a1a9a08e06c82a65ebd3019ca6f6f7dd8b2c73
Rahul-Mehar <rahulmehar92@gmail.com>
2025-01-28 14:17:45 +0530

    update

---
+++ 02_basics/objects.js
@@ -0,0 +1,20 @@
+// object declare by two ways
+//1 literal
+//2 constructor
+
+// singleton you made by any constructor 
+//object.create // this is made by constructor
+
+
+
+
+//object   Literals
+const Jsuser = {
+    name: "Rahul",
+
+}
+
+
+myArray = ["R", "A"]
+myArray[0];
+
