67761ccfb64bc009946a0957ee0bb1e489ab0005
Rahul-Mehar <rahulmehar92@gmail.com>
2025-01-28 18:03:30 +0530

    update

--- 02_basics/objects.js
+++ 02_basics/objects.js
@@ -9,12 +9,56 @@
 
 
 //object   Literals
+
+const mySym = Symbol("key1")
+
 const Jsuser = {
     name: "Rahul",
+    "full name": "Rahul Mehar",
+    [mySym]: "key1",
+    age: 28,
+    location: "pune",
+    email: "rahulmehar22@gmail.com",
+    isLoggedIn: false,
+    lastLoginDays: ["Monday", "Tuesday"]
 
 }
 
+// console.log(Jsuser.location);
+// console.log(Jsuser["email"]);
+// console.log(Jsuser.email);
+// console.log(Jsuser["full name"]);
+// console.log(Jsuser[mySym]);
+// console.log(typeof Jsuser.mySym);
 
-myArray = ["R", "A"]
-myArray[0];
+
+// over write and change the value also
+
+Jsuser.email = "Rahulmehar897@gmail.com";
+// Object.freeze(Jsuser)// by this method now no one change there orignal value
+
+// Jsuser.name = "Mohit";
+
+// console.log(Jsuser);
+
+
+Jsuser.greeting = function(){
+    console.log("Hello js user");
+    
+}
+
+Jsuser.greetingTwo = function(){
+    console.log(`Hello js user, ${this.name}`);
+    
+}
+
+
+
+console.log(Jsuser.greetingTwo());
+
+
+
+
+// myArray = ["R", "A"]
+// myArray[0];
 
