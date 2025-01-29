9efc71335ba83d8dede6f3d5fd06290281892f69
Rahul-Mehar <rahulmehar92@gmail.com>
2025-01-29 12:57:43 +0530

    update

--- 02_basics/objectTwo.js
+++ 02_basics/objectTwo.js
@@ -1,10 +1,62 @@
 
 
 
-const tinderUser = new Object()
+// const tinderUser = new Object()
 
- console.log(tinderUser);
+const tinderUser = {}
 
+tinderUser.id = "123abc"
+tinderUser.name = "Rahul"
+tinderUser.isloggedin = "falsec"
+
+//  console.log(tinderUser);
+
+
+ const regularUser = {
+    email: "rahul@gmail.com",
+    fullname:{
+        userfullname:{
+            firstname: "Rahul",
+            lastname: "Mehar"
+        }
+    }
+ }
+
+//  console.log(regularUser);
+//  console.log(regularUser.fullname.userfullname.firstname);
+
+
+const obj1 = {1: "a", 2:"b"}
+const obj2 = {3: "a", 4:"b"}
+const obj4 = {5: "a", 6:"b"}
+
+// const obj3 = {...obj1, ...obj2, ...obj4}
+// const obj3 = {obj1, obj2}
+// const obj3 = Object.assign({}, obj1, obj2, obj4)
+
+// console.log(obj3);
+
+
+const users = [
+     {
+        id: 1,
+        email: "rahul@gmail.com"
+     },
+     {
+        id: 2,
+        email: "sumit@gmail.com"
+     },
+     {
+        id: 3,
+        email: "chataniya@gmail.com"
+     },
+     {
+        id: 4,
+        email: "mohit@gmail.com"
+     }
+    ]
+
+    users[1].email
 
 
 
