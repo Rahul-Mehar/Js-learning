6bc6d5b263c0b941ad5cf8a484e5559cae209677
Rahul-Mehar <rahulmehar92@gmail.com>
2025-01-31 02:21:08 +0530

    update

--- 03_basics/Scopes.js
+++ 03_basics/Scopes.js
@@ -7,15 +7,88 @@
 
 let a = 300;
 
-if(true){
-    let a = 10
-    const b = 20
-    console.log("Inner: ", a);
-    
-    
-}
+// if(true){
+//     let a = 10
+//     const b = 20
+//     console.log("Inner: ", a);
+
+// }
 
 
-console.log(a);
+// console.log(a);
 // console.log(b);
 // console.log(c);
+
+
+// nested scope
+
+function one(){
+    const username = "rahul"
+    function two(){
+        const website = "youtube"
+        // console.log(username); // rahul
+    }
+    // console.log(website);// error because we access it out of the scope
+
+    two()
+}
+
+one() // no result beacuse now we are not calling two function thats why
+
+
+if (true){
+    const username = "Rahul"
+ if(username === "Rahul"){
+    const website = "youtube"
+    // console.log(username + website); // Rahul youtube
+    
+ }
+// console.log(website); // error website is not defined
+
+
+}
+
+// console.log(username);// error username is not defined
+
+
+
+//****interesting****
+
+function addone(num){
+    return num + 1
+}
+// addone(5) 
+
+
+
+const addTwo =  function(num){
+    return num + 2
+}
+addTwo(5)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
