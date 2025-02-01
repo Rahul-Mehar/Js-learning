0fdb3f76be8da74080bfd1cfde6ff0f3f8356ad0
Rahul-Mehar <rahulmehar92@gmail.com>
2025-02-01 10:32:56 +0530

    IIFE

---
+++ 03_basics/IIFE.js
@@ -0,0 +1,32 @@
+
+//  immediately invoked function Expression (IIFE)
+
+
+// (function chai(){
+// named IIFE
+//     console.log("DB Connected");
+    
+// })();
+
+// chai()
+// ()()
+
+// ( ()=>{
+//     console.log(`DB CONNECTED`);
+    
+// } )()
+
+
+
+// ( function aurcode (name){
+//     console.log(`DB CONNECTED Two ${name}`);
+    
+// } )('Rahul');
+
+
+//**********interview question
+// 1. write a two IIFE?
+// 2. write a named/ unnamed IIFE?
+// 3. write a simple IIFE?
+
+
