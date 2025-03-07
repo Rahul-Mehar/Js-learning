1c1df0e6b324d071421ed4fe75448099aa1db213
Rahul-Mehar <rahulmehar92@gmail.com>
2025-03-07 12:00:47 +0530

    update

--- 05_iterrations/one.js
+++ 05_iterrations/one.js
@@ -1,11 +1,59 @@
 //  for
 
-for (let i = 0; i <= 10; i++) {
-    const element = i;
-    if(element == 5){
-        console.log("5 is best number");
+// for (let i = 0; i <= 10; i++) {
+//     const element = i;
+//     if(element == 5){
+//         console.log("5 is best number");
         
-    }
-    console.log(element);
+//     }
+//     console.log(element);
     
+// }
+
+// for (let i = 0; i <= 10 ; i++) {
+//     const element = i;
+//     if (element == 5) {
+//         console.log("5  is the best number");
+        
+//     }
+//     console.log(element);
+    
+// }
+
+
+// for (let i = 1; i <= 10; i++) {
+//     console.log(`Outer loop value: ${i}`);
+    
+//     for (let j = 1; j <= 10; j++) {
+//         const element = j;
+//         // console.log(`Inner loop value: ${j} & ${i}`);
+//         console.log(i + '*' + j + '=' + i*j);
+        
+//     }
+    
+// }
+
+
+// let Myarray = ["flash", "batman", "superman"]
+// console.log(Myarray.length);
+// for (let index = 0; index < Myarray.length; index++) {
+//     const element = Myarray[index];
+//     console.log(element);
+// }
+
+
+// break and continue
+
+
+for (let i = 1; i <= 20; i++){
+    if(i == 5){
+        console.log(`Detected 5`);
+        // break
+        continue
+    }
+    
+    console.log(`value of i is :${i}`);
+    
+   
 }
+
---
+++ 05_iterrations/two.js
@@ -0,0 +1,51 @@
+
+// let i = 0
+// while (i <=10) {
+//     console.log(`value of i is ${i}`);
+//     i = i + 2
+//     // here we increase by 2 that why its give 0,2,4,6,8,10 and zero its initial value
+// }
+
+
+
+
+// let array = ['flash', 'batman', 'superman']
+
+// let arr = 0
+// while (arr < array.length) {
+//     console.log(`value is ${array[arr]}`);
+//     arr = arr + 1;
+    
+// }
+
+
+// do while loop mai phle kam hoga phr condition check hogi islye do while loop use krtey hai
+
+let score = 1 // ans is 1,2,3,4,5,6,7,8,10
+
+// let score = 11 // here  ans is 11 beacuse first do then check the condition
+
+
+do {
+    console.log(`Score is ${score}`);
+    score ++  ;
+} while (score <= 10 );
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
